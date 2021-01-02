cfa03bb87227805b3cd1fd0de5e24733b8a0effb
Patrick Tuszakowski <mrtall500@gmail.com>
2021-01-02 03:00:59 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -202,12 +202,12 @@ end
 
 def big_shoe_rebounds
   big_shoes = 0
-  robounds = 0
+  rebounds = 0
   game_hash.each do |location, team|
     team[:players].each do |stats|
       if stats[:shoe] > big_shoes
         big_shoes = stats[:shoe]
-        rebounds = stats[:rebound]
+        rebounds = stats[:rebounds]
       end
     end
   end
